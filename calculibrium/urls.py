7d9e650da32487f33e6d9c794b18dfe110505ffa
rafachang <108372442+rafachang@users.noreply.github.com>
2023-11-06 09:43:15 -0300

    Some changes

--- calculibrium/urls.py
+++ calculibrium/urls.py
@@ -6,5 +6,6 @@ app_name = 'calculibrium'
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:categoria_componente>/list", views.list, name="list"),
-    path("structure", views.structure, name="structure"),
+    path("structure/kwp", views.structure_kwp, name="structure_kwp"),
+    path("structure/un", views.structure_un, name="structure_un"),
 ]
--- calculibrium/views.py
+++ calculibrium/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.core.handlers.wsgi import WSGIRequest
+from django.core.exceptions import FieldError
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
@@ -19,7 +20,7 @@ def list(request: WSGIRequest, categoria_componente):
         raise Http404('Component does not exist!')
     return render(request, "calculibrium/list.html", {"list": list})
 
-def structure(request: WSGIRequest):
+def structure_kwp(request: WSGIRequest):
     power_plant = PowerPlant('Rafael Chang', Module(0,0,0,0,0,0,0,0,540,2261,1134,35,0,0,0,0), 103.68)
     if request.method == 'POST':
         items = {}
@@ -34,18 +35,33 @@ def structure(request: WSGIRequest):
         power_plant.calc_concrete(1.6, 0.15, 0.4, 0.2)
         
         
-    return render(request, "calculibrium/structure.html", {"power_plant": power_plant})
+    return render(request, "calculibrium/structure_kwp.html", {"power_plant": power_plant})
 
-# txt_customers_name
-# txt_module_lenght
-# txt_module_power
-# txt_power_plant_power
-# chk_inverter
-# txt_height_buried
-# txt_ray_buried
-# txt_volume_buried
-# txt_height_exposed
-# txt_ray_exposed
-# txt_volume_exposed
-# "calculate"
-# "export"
+def structure_un(request: WSGIRequest):
+    power_plant = PowerPlant('Rafael Chang', Module(0,0,0,0,0,0,0,0,540,2261,1134,35,0,0,0,0), 0)
+    if request.method == 'POST':
+        items = {}
+        for chave, valor in request.POST.items():
+            items[chave] = valor
+        try:
+            modules_amount = [int(i) for i in request.POST.getlist('txt_modules_amount')]
+            tables_amount = [int(i) for i in request.POST.getlist('txt_tables_amount')]
+        except:
+            raise FieldError('Calculo não realizado, verifique os campos e tente novamente!')
+        power_plant = PowerPlant(items['txt_customers_name'], 
+                                 Module(0,0,0,0,0,0,0,0,int(items['txt_module_power']), 2261, int(items['txt_module_width']), 35, 0, 0, 0, 0),
+                                 0)
+        power_plant.modules_amount = sum([int(i)*int(tables_amount[idx]) for idx, i in enumerate(modules_amount)])
+        power_plant.real_power = power_plant.modules_amount*power_plant.module.potencia/1e3
+        power_plant.table.tables['qtd_modulo'] = modules_amount
+        power_plant.table.tables['qtd_mesas'] = tables_amount
+        power_plant.table.tables['dimensions'] = None
+        power_plant.inverter_table = True if request.POST.get('chk_inverter') else False
+        power_plant.calc_structure()
+        power_plant.calc_concrete(1.6, 0.15, 0.4, 0.2)
+        return render(request, "calculibrium/structure_un.html", {
+            "power_plant": power_plant,
+            "tables": zip(modules_amount, tables_amount)
+        })
+
+    return render(request, "calculibrium/structure_un.html", {"power_plant": power_plant})
